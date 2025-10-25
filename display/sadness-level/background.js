0a2b0550f5922d58e2c9d81cd4d70d99b7c25520
Zainab <zbsyed05@gmail.com>
2025-10-25 14:17:57 -0400

    Create background.js

---
+++ display/sadness-level/background.js
@@ -0,0 +1,17 @@
+this.camera.main.setBackgroundColor(0x1a11e2b);
+this.player.setTint(0x4f5b7a);
+
+export class Game extends Phaser.scene {
+    constructor() {
+        super('Game');
+
+    }
+
+    create() {
+
+    }
+
+    update() {
+
+    }
+}
